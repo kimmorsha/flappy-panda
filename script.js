8a1b629f2be5cd2b950b23073ae52f6db7ed610d
arpacada <arpacada@up.edu.ph>
2017-05-30 22:09:28 +0800

    2 bamboos and ajusted up and down

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $(function () {
 
     //saving dom objects to variables
@@ -5,9 +6,12 @@ $(function () {
     //cause we are poor huhuhu
     var container = $('#container');
     var panda = $('#panda');
-    var pole = $('.pole');
+    var pole1 = $('.pole1');
+    var pole2 = $('.pole2')
     var pole_1 = $('#pole_1');
     var pole_2 = $('#pole_2');
+    var pole_3 = $('#pole_3');
+    var pole_4 = $('#pole_4');
     var score = $('#score');
     var speed_span = $('#speed');
     var restart_btn = $('#restart_btn');
@@ -15,8 +19,9 @@ $(function () {
     //saving some initial setup
     var container_width = parseInt(container.width());
     var container_height = parseInt(container.height());
-    var pole_initial_position = parseInt(pole.css('right'));
-    var pole_initial_height = parseInt(pole.css('height'));
+    var pole1_initial_position = parseInt(pole1.css('right'));
+    var pole2_initial_position = parseInt(pole2.css('right'));
+    var pole_initial_height = parseInt(pole1.css('height'));
     var panda_left = parseInt(panda.css('left'));
     var panda_height = parseInt(panda.height());
     var speed = 10;
@@ -24,39 +29,47 @@ $(function () {
     //some other declarations
     var go_up = false;
     var score_updated = false;
+    var score2_updated = false;
     var game_over = false;
 
 
     var the_game = setInterval(function(){
 
-        if (collision(panda, pole_1) || collision(panda, pole_2) ||
+        if (collision(panda, pole_1) || collision(panda, pole_2) || collision(panda, pole_3) ||collision(panda, pole_4) ||
           parseInt(panda.css('top')) <= 0 ||
           (parseInt(panda.css('top')) > (container_height - panda_height))) {
 
           stop_the_game();
 
         } else {
-          var pole_current_position = parseInt(pole.css('right'));
+          var pole1_current_position = parseInt(pole1.css('right'));
+          var pole2_current_position = parseInt(pole2.css('right'));
 
           // update the score when the poles have been passed by the panda
-          if (pole_current_position > (container_width - panda_left)) {
+          if (pole1_current_position > (container_width - panda_left)) {
               if (score_updated === false) {
                 score.text(parseInt(score.text()) + 1);
                 score_updated = true;
               }
           }
+          if(pole2_current_position > (container_width - panda_left)){
+            if (score2_updated === false) {
+                score.text(parseInt(score.text()) + 1);
+                score2_updated = true;
+            }
+          }
 
 
           //check whether the poles went out of the container
-          if (pole_current_position > container_width) {
+          if (pole1_current_position > container_width) {
               //we need to change the pole's height as well
-              var new_height = parseInt(Math.random() * 100);
+              var new_height = parseInt((Math.random() * 100) + 50);
 
               pole_1.css('height', pole_initial_height + new_height);
               pole_2.css('height', pole_initial_height - new_height)
 
               //increase the speed
-              speed = speed + 1;
+              //speed = speed + 1;
               speed_span.text(speed);
 
               score_updated = false;
@@ -64,7 +77,29 @@ $(function () {
               //just before the pole is rest the height of the poles is changed
               //if it is out of the container then we need to..
               //bring it back to its first position
-              pole_current_position = pole_initial_position;
+              pole1_current_position = pole1_initial_position;
+
+              if (go_up === false) {
+                  go_down();
+              };
+          };
+           if (pole2_current_position > container_width) {
+              //we need to change the pole's height as well
+              var new_height = parseInt((Math.random() * 100) + 50);
+
+              pole_3.css('height', pole_initial_height + new_height);
+              pole_4.css('height', pole_initial_height - new_height)
+
+              //increase the speed
+              speed = speed + 1;
+              speed_span.text(speed);
+
+              score2_updated = false;
+
+              //just before the pole is rest the height of the poles is changed
+              //if it is out of the container then we need to..
+              //bring it back to its first position
+              pole2_current_position = pole2_initial_position;
 
               if (go_up === false) {
                   go_down();
@@ -72,7 +107,8 @@ $(function () {
           };
 
           //move the poles
-          pole.css('right', pole_current_position + speed);
+          pole1.css('right', pole1_current_position + speed);
+          pole2.css('right', pole2_current_position + speed);
 
           // to make the default move go down if even space is not pressed
           if (go_up === false) {
@@ -100,13 +136,13 @@ $(function () {
     function go_down () {
         //it is going to move the panda down
         //by increasing the top
-        panda.css('top', parseInt(panda.css('top')) + 5);
+        panda.css('top', parseInt(panda.css('top')) + 9.8);
     }
 
     function up() {
         //it is going to move the panda up
         //by decreasing the top
-        panda.css('top', parseInt(panda.css('top')) - 10);
+        panda.css('top', parseInt(panda.css('top')) - 15);
     }
 
     function stop_the_game(){
