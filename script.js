2fe4f79e66299ff8ebaac19d060aae7880eb3f56
arpacada <arpacada@up.edu.ph>
2017-05-31 09:57:47 +0800

    2 bamboos 2 walls

--- script.js
+++ script.js
@@ -7,21 +7,34 @@ $(function () {
     var container = $('#container');
     var panda = $('#panda');
     var pole1 = $('.pole1');
-    var pole2 = $('.pole2')
+    var pole2 = $('.pole2');
+    var pole3 = $('.pole3');
+    var pole4 = $('.pole4');
     var pole_1 = $('#pole_1');
     var pole_2 = $('#pole_2');
     var pole_3 = $('#pole_3');
     var pole_4 = $('#pole_4');
+    var pole_5 = $('#pole_6');
+    var pole_6 = $('#pole_5');
+    var pole_7 = $('#pole_7');
+    var pole_8 = $('#pole_8');
     var score = $('#score');
     var speed_span = $('#speed');
     var restart_btn = $('#restart_btn');
+
 
     //saving some initial setup
     var container_width = parseInt(container.width());
     var container_height = parseInt(container.height());
     var pole1_initial_position = parseInt(pole1.css('right'));
     var pole2_initial_position = parseInt(pole2.css('right'));
+    var pole3_initial_position = parseInt(pole3.css('right'));
+    var pole4_initial_position = parseInt(pole4.css('right'));
     var pole_initial_height = parseInt(pole1.css('height'));
+    var pole3_top = parseInt(pole_5.css('top'));
+    var pole3_bottom = parseInt(pole_6.css('bottom'));
+      var pole4_top = parseInt(pole_7.css('top'));
+    var pole4_bottom = parseInt(pole_8.css('bottom'));
     var panda_left = parseInt(panda.css('left'));
     var panda_height = parseInt(panda.height());
     var speed = 10;
@@ -30,12 +43,15 @@ $(function () {
     var go_up = false;
     var score_updated = false;
     var score2_updated = false;
+    var passed3 = false;
+    var passed4 = false;
     var game_over = false;
 
 
     var the_game = setInterval(function(){
 
-        if (collision(panda, pole_1) || collision(panda, pole_2) || collision(panda, pole_3) ||collision(panda, pole_4) ||
+        if (collision(panda, pole_1) || collision(panda, pole_2) || collision(panda, pole_3) ||collision(panda, pole_4) || collision(panda, pole_5)||
+          collision(panda, pole_6) || collision(panda, pole_7) ||collision(panda, pole_8)||
           parseInt(panda.css('top')) <= 0 ||
           (parseInt(panda.css('top')) > (container_height - panda_height))) {
 
@@ -44,6 +60,8 @@ $(function () {
         } else {
           var pole1_current_position = parseInt(pole1.css('right'));
           var pole2_current_position = parseInt(pole2.css('right'));
+          var pole3_current_position = parseInt(pole3.css('right'));
+          var pole4_current_position = parseInt(pole4.css('right'));
 
           // update the score when the poles have been passed by the panda
           if (pole1_current_position > (container_width - panda_left)) {
@@ -56,6 +74,18 @@ $(function () {
             if (score2_updated === false) {
                 score.text(parseInt(score.text()) + 1);
                 score2_updated = true;
+            }
+          }
+          if(pole3_current_position > (container_width - panda_left)) {
+            if(!passed3) {
+              score.text(parseInt(score.text()) + 1);
+              passed3 = true;
+            }
+          }
+            if(pole4_current_position > (container_width - panda_left)) {
+            if(!passed3) {
+              score.text(parseInt(score.text()) + 1);
+              passed4 = true;
             }
           }
 
@@ -85,7 +115,7 @@ $(function () {
           };
            if (pole2_current_position > container_width) {
               //we need to change the pole's height as well
-              var new_height = parseInt((Math.random() * 100) + 50);
+              var new_height = parseInt((Math.random() * 100) + (-50));
 
               pole_3.css('height', pole_initial_height + new_height);
               pole_4.css('height', pole_initial_height - new_height)
@@ -106,9 +136,39 @@ $(function () {
               };
           };
 
+          if( pole3_current_position > container_width) {
+            var new_height = parseInt((Math.random() * 100) + (-100));
+
+            pole_5.css('top', pole3_top - new_height);
+            pole_6.css('bottom', pole3_bottom + new_height);
+
+            passed3 = false;
+            pole3_current_position= pole3_initial_position;
+
+            if(go_up ===false) {
+              go_down();
+            }
+          }
+           if( pole4_current_position > container_width) {
+            var new_height = parseInt((Math.random() * 100) + (-100));
+
+            pole_7.css('top', pole4_top - new_height);
+            pole_8.css('bottom', pole4_bottom + new_height);
+
+            passed4 = false;
+            pole4_current_position= pole4_initial_position;
+
+            if(go_up ===false) {
+              go_down();
+            }
+          }
+
+
           //move the poles
           pole1.css('right', pole1_current_position + speed);
           pole2.css('right', pole2_current_position + speed);
+          pole3.css('right', pole3_current_position + speed);
+          pole4.css('right', pole4_current_position + speed);
 
           // to make the default move go down if even space is not pressed
           if (go_up === false) {
@@ -132,11 +192,72 @@ $(function () {
             go_up = false;
         };
     });
+//     var panda_Image = new Image();
+//     panda_Image.src = "pandasprite.png";
 
+//     var canvas = document.getElementById("pandaAnimation");
+//     canvas.width = 100;
+//     canvas.height = 100;
+
+//     var panda_ = sprite({
+//         context : canvas.getContext("2d");
+//         width:100,
+//         height:100,
+//         image: panda_Image
+//     });
+//     panda_.render();
+//     function sprite (options) {
+                    
+//     var that = {},
+//         that.context = options.context;
+//         that.width = options.width;
+//         that.height = options.height;
+//         that.image = options.image;
+//         frameIndex = 0,
+//         tickCount = 0,
+//         ticksPerFrame = 0,
+//         numberOfFrames = options.numberOfFrames || 1;
+
+//           that.loop = options.loop;
+//           that.render = function () {
+          
+//     // Clear the canvas
+//            context.clearRect(0, 0, that.width, that.height);
+
+//         // Draw the animation
+//            that.context.drawImage(
+//            that.image,
+//            frameIndex * that.width / numberOfFrames,
+//            0,
+//            that.width,
+//            that.height,
+//            0,
+//            0,
+//            that.width,
+//            that.height);
+//        };   
+// };
+
+   
+    
+
+
+// }
+
+// function gameLoop () {
+
+//   window.requestAnimationFrame(gameLoop);
+  
+//   coin.update();
+//   coin.render();
+// }
+
+// // Start the game loop as soon as the sprite sheet is loaded
+// coinImage.addEventListener("load", gameLoop);
     function go_down () {
         //it is going to move the panda down
         //by increasing the top
-        panda.css('top', parseInt(panda.css('top')) + 9.8);
+        panda.css('top', parseInt(panda.css('top')) + 9);
     }
 
     function up() {
